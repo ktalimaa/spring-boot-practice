a8f01f45ff3577ac933ad528d9b2ff78beb84600
ktalimaa <kristeltalimaa@gmail.com>
2023-02-27 19:32:01 +0200

    Spring audit implementation

--- src/main/java/com/sda/study/springbootpractice/configurations/AuditConfiguration.java
+++ src/main/java/com/sda/study/springbootpractice/configurations/AuditConfiguration.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Optional;
-
 /**
  * Configuration for auditing
  */
